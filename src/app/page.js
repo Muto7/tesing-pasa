de7fffc9554e61daf91690fde4e808d23759076c
Mucamad supriyanto <msupri29@gmail.com>
2025-06-11 23:40:06 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,7 @@ import UsersPage from "./users/page";
 export default function Home() {
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex-1 ml-64 p-4 mt-16">
-        <UsersPage />
-      </div>
+      <UsersPage />
     </div>
   );
 }
