105f3c961a6272cf2ec5531a3caaeac8fd1e757e
Mucamad supriyanto <msupri29@gmail.com>
2025-06-11 23:51:36 +0700

    update

--- src/app/components/Utilities/Sidebottom.js
+++ src/app/components/Utilities/Sidebottom.js
@@ -1,23 +1,24 @@
 import { User, BookOpenText } from "lucide-react";
+import Link from "next/link";
 
 export default function Sidebuttom() {
   return (
     <div className="flex justify-around ">
       <div className="bg-green">
-        <a
+        <Link
           href="/users"
           className="flex items-center space-x-3 hover:bg-indigo-600 px-3 py-2 rounded mr-7"
         >
           <User className=" w-10 h-10" />
-        </a>
+        </Link>
       </div>
       <div className="flex justify-center">
-        <a
+        <Link
           href="/blog"
           className="flex items-center space-x-3 hover:bg-indigo-600 px-3 py-2 rounded ml-10"
         >
           <BookOpenText className="w-10 h-10" />
-        </a>
+        </Link>
       </div>
     </div>
   );
